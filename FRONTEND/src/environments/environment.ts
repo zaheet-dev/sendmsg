e41bcafef11052ae125fa5566b82bfcbe7db3196
zaheet-fitterfly <73466205+zaheet-fitterfly@users.noreply.github.com>
2021-09-14 12:21:09 +0530

    updated heroku creds

--- FRONTEND/src/environments/environment.ts
+++ FRONTEND/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl : "https://cryptic-lowlands-61116.herokuapp.com/api"
-  apiUrl : "http://localhost:3000/api" 
+  apiUrl : "https://cryptic-lowlands-61116.herokuapp.com/api"
+  // apiUrl : "http://localhost:3000/api" 
 };
 
 /*
